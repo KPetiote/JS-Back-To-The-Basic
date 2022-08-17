ee4f4c80bb2700da94d6d75a8de0130db28387f4
Kenneth Petiote <kpetiote@ac.com>
2022-08-17 16:22:57 -0400

    Testing out the console - variables, arrays, tables

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 alert("Hello World");
 // Log to console
-console.log('Hello World')
-console.log(123)
-console.log(true)
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
 /*
   Multi
   Line
