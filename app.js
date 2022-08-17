ae4f38bd0dd24cd344f849f1d65f988c901da4f3
Kenneth Petiote <kpetiote@ac.com>
2022-08-17 16:28:19 -0400

    Testing out the console - erro, warn, time, timeEnd

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ console.log(greeting);
 console.log([1,2,3,4]);
 console.log({a:1, b:2});
 console.table({a:1, b:2});
+
+console.error('This is some error');
+//console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
 /*
   Multi
   Line
