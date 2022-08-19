dfa8c0543df7bf41bdec95d9d2ec070930da2d2a
Kenneth Petiote <kpetiote@ac.com>
2022-08-19 09:27:28 -0400

    Testing out VAR

--- variables.js
+++ variables.js
@@ -1,4 +1,24 @@
 // var, let, const
 
-var name = 'John Doe';
-console.log(name);
+var testname = 'John Doe';
+console.log(testname);
+testname = 'Steve Smith';
+console.log(testname);
+
+// init var
+
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+/* 
+Variables can include -= letters, numbes, _, $
+Variables cannot start with a number 
+*/
+
+// Multi Word Variables
+var firstName = 'John'; // Camel Case - Recommended
+var first_name = 'Sara'; // Underscore - Seein in PHP
+var FirstName = 'Tom'; // Pascal Case - Object Oriented / Constructive Function / ES6 Classes
+var firstname; // Not Recommended
