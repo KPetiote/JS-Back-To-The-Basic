2c821cb1a1508357794d1d28a6f4b7239a10c890
Kenneth Petiote <kpetiote@ac.com>
2022-08-25 17:13:17 -0400

    Testing out Data Types

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+// 6 PRIMITIVE TYPES
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+//Symbol
+const sym = Symbol();
+
+// 3 REFERENCE TYPES - Objects
+
+// Array
+const hobbies = ['movies','music'];
+// Object Literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+} 
+// Date
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
