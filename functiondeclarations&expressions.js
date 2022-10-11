fc623331b71447ce7007cf2180d0282dc9ce4abf
Kenneth Petiote <kenneth.petiote@gmail.com>
2022-10-11 18:51:57 -0400

    testing out property methods in functiondeclarations&expressions.js

--- functiondeclarations&expressions.js
+++ functiondeclarations&expressions.js
@@ -29,3 +29,22 @@ const square = function(x = 3){
 // (function(name){
 //     console.log('Hello '+ name);
 // })('Brad');
+
+// PROPERTY METHODS
+
+const todo = {
+    add: function(){
+        console.log('Add to do...'); 
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`);
+    }
+}
+
+todo.delete = function(){
+    console.log('Delete todo....');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
